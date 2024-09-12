3e2929085a66f62ad96cbb16cbffa483b5b26134
Bad_Santa <denisen@bk.ru>
2024-09-12 12:26:46 +0600

    Обновленная версия
    От 12092024

--- SQL_2_HW_2.sql
+++ SQL_2_HW_2.sql
@@ -52,36 +52,40 @@ INSERT INTO compilation_track (compilation_id, track_id) VALUES (3, 6);
 INSERT INTO compilation_track (compilation_id, track_id) VALUES (4, 1);
 INSERT INTO compilation_track (compilation_id, track_id) VALUES (4, 3);
 
-
-SELECT-запросы
-
+-- SELECT-запросы
 
 -- Название и продолжительность самого длительного трека
+
 SELECT title, duration
 FROM track
 ORDER BY duration DESC
 LIMIT 1;
 
-
 -- Название треков, продолжительность которых не менее 3,5 минут (210 секунд)
+
 SELECT title
 FROM track
 WHERE duration >= 210;
 
 
 -- Названия сборников, вышедших в период с 2018 по 2020 год включительно
+
 SELECT title
 FROM compilation
 WHERE release_year BETWEEN 2018 AND 2020;
 
 
 -- Исполнители, чьё имя состоит из одного слова
+
 SELECT name
 FROM artist
 WHERE name NOT LIKE '% %';
 
 -- Название треков, которые содержат слово «мой» или «my»
 
+-- Для корректного поиска треков, содержащих слово "мой" или "my" как отдельное слово, используем регулярные выражения:
+
 SELECT title
 FROM track
-WHERE title ILIKE '%мой%' OR title ILIKE '%my%';
+WHERE title ~* '\yмой\y' -- слово "мой" как отдельное слово
+OR title ~* '\ymy\y'; -- слово "my" как отдельное слово
